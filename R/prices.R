ea5e20197c8e00eae784017efc3c1e54bb8bde6b
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-09-12 15:17:12 +0800

    Fixing typos

--- R/prices.R
+++ R/prices.R
@@ -1,10 +1,10 @@
-#' Set of random selections of 50 stocks from the S&P 500
+#' Set of random selections of 50 stocks from the S&P 500 during periods of two years
 #'
 #' @docType data
 #'
 #' @usage data(prices)
 #'
-#' @format A list of \code{xts} objects with the random selection of 50 stock price during random periods of two years.
+#' @format A list of \code{xts} objects with the prices of random selections of 50 stocks from the S&P 500 during periods of two years.
 #'
 #' @source \href{https://finance.yahoo.com}{Yahoo! Finance}
 #'
