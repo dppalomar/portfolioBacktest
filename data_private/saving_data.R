907118aa6f923cc1ce8fec7eb2e0db0d9b62551e
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-09-13 09:41:25 +0800

    Fix data that was supposed to be prices and it was returns!

--- data_private/saving_data.R
+++ data_private/saving_data.R
@@ -8,30 +8,30 @@ T_sample <- 2*252  #2 years of data
 prices <- list()
 
 load("data_private/stocks_SP500_1999_2005.RData")
-X <- data_1999_2005$X
-N <- ncol(X)
-T <- nrow(X)
+prices_ <- exp(cumsum(data_1999_2005$X))
+N <- ncol(prices_)
+T <- nrow(prices_)
 for(i in 1:10) {
   t_start <- sample(T-T_sample+1, 1)
-  prices <- c(prices, list(X[t_start:(t_start+T_sample-1), sample(N, 50)]))
+  prices <- c(prices, list(prices_[t_start:(t_start+T_sample-1), sample(N, 50)]))
 }
 
 load("data_private/stocks_SP500_2006_2012.RData")
-X <- data_2006_2012$X
-N <- ncol(X)
-T <- nrow(X)
+prices_ <- exp(cumsum(data_2006_2012$X))
+N <- ncol(prices_)
+T <- nrow(prices_)
 for(i in 1:10) {
   t_start <- sample(T-T_sample+1, 1)
-  prices <- c(prices, list(X[t_start:(t_start+T_sample-1), sample(N, 50)]))
+  prices <- c(prices, list(prices_[t_start:(t_start+T_sample-1), sample(N, 50)]))
 }
 
 load("data_private/stocks_SP500_2010_2015.RData")
-X <- data_2010_2015$X
-N <- ncol(X)
-T <- nrow(X)
+prices_ <- exp(cumsum(data_2010_2015$X))
+N <- ncol(prices_)
+T <- nrow(prices_)
 for(i in 1:10) {
   t_start <- sample(T-T_sample+1, 1)
-  prices <- c(prices, list(X[t_start:(t_start+T_sample-1), sample(N, 50)]))
+  prices <- c(prices, list(prices_[t_start:(t_start+T_sample-1), sample(N, 50)]))
 }
 
 object.size(prices)
