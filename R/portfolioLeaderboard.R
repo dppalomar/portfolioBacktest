6c4feadeb492351c5e944df834efd1a4e921239a
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-09-20 23:15:31 +0800

    Leaderboard computation revised... Scores are now between 0 and 100.

--- R/portfolioLeaderboard.R
+++ R/portfolioLeaderboard.R
@@ -15,30 +15,24 @@ portfolioLeaderboard <- function(res = NA, weights = c(1, 1, 1, 7)) {
   if (length(weights) != 4) stop("argument \"weights\" must have 4 elements")
   
   # sort the vaild scores
-  weights <- 100 * weights / (sum(weights))
+  weights <- weights / (sum(weights))
   mask_valid <- res$failure_ratio != 1
-  scores <- cbind(percentile(res$performance_summary[mask_valid, 1]),
-                  percentile(-res$performance_summary[mask_valid, 2]),
-                  percentile(-res$cpu_time_average[mask_valid]),
-                  percentile(-res$failure_ratio[mask_valid]))
-  scores <- scores * matrix(weights, nrow(scores), ncol(scores), byrow = TRUE)
-  performance <- scores %*% cbind(rep(1, ncol(scores)))
-  
-  index_sorting <- sort(performance, decreasing = TRUE, index = TRUE)$ix
+  scores <- cbind(rank_percentile(res$performance_summary[mask_valid, 1]),
+                  rank_percentile(-res$performance_summary[mask_valid, 2]),
+                  rank_percentile(-res$cpu_time_average[mask_valid]),
+                  rank_percentile(-res$failure_ratio[mask_valid]))
+  final_score <- scores %*% weights
+  index_sorting <- sort(final_score, decreasing = TRUE, index = TRUE)$ix
   
   # combine the valid and invalid scores
-  leaderboard_vaild <- cbind(scores[index_sorting, ], performance[index_sorting])
-  leaderboard_invai <- matrix(NA, sum(!mask_valid), 5)
-  leaderboard <- rbind(leaderboard_vaild, leaderboard_invai)
+  leaderboard_valid <- cbind(scores[index_sorting, ], final_score[index_sorting])
+  leaderboard_invalid <- matrix(NA, sum(!mask_valid), 5)
+  leaderboard <- rbind(leaderboard_valid, leaderboard_invalid)
   
   # add names
   index_vaild_sorted <- (1:length(mask_valid))[mask_valid][index_sorting]
   index_sorted <- c(index_vaild_sorted, (1:length(mask_valid))[-index_vaild_sorted])
-  colnames(leaderboard) <- c("sharpe ratio rank percentile", 
-                             "max drawdown rank percentile", 
-                             "cpu time rank percentile", 
-                             "failure ratio rank percentile", 
-                             "final rank percentile")
+  colnames(leaderboard) <- c("sharpe ratio score", "max drawdown score", "cpu time score", "failure ratio score", "final score")
   if (!is.null(res$stud_IDs)){
     stud_info <- cbind(res$stud_names[index_sorted], res$stud_IDs[index_sorted])
     rownames(leaderboard) <- stud_info[, 2]
@@ -51,7 +45,10 @@ portfolioLeaderboard <- function(res = NA, weights = c(1, 1, 1, 7)) {
   }
 }
 
-percentile <- function(score) {
-  ecdf_ <- ecdf(score)
-  return(ecdf_(score))
+rank_percentile <- function(x) {
+  N <- length(x)
+  rank_pctl <- ecdf(x)(x)
+  rank_pctl <- (rank_pctl - 1/N)/(1 - 1/N)
+  return (100*rank_pctl)
 }
+  
