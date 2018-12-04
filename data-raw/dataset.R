7b33bd84a180518716fbc43c5771004a9e00a716
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-12-04 16:52:06 +0800

    To do

--- data-raw/dataset.R
+++ data-raw/dataset.R
@@ -1,18 +1,7 @@
+# download data from Yahoo!Finance
 
-#generate heavy-tailed data and save it
-N <- 40
-T <- 1000
-mu <- rep(0,N)
-nv <- 4
-U <- t(mvtnorm::rmvnorm(n=round(0.7*N), sigma=0.1*diag(N)))
-R_cov <- U %*% t(U) + diag(N)
-X <- mvtnorm::rmvt(n=T, delta=mu, sigma=(nv-2)/nv*R_cov, df=nv)
 
-heavy_data <- list(N = N,
-                   T = T,
-                   mu = mu,
-                   cov = R_cov,
-                   nv = nv,
-                   X = X)
+# save it in the variable dataset
 
-devtools::use_data(heavy_data, overwrite = TRUE)
+# save dataset to the data folder
+devtools::use_data(dataset, overwrite = TRUE)
