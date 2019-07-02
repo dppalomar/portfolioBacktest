c1c2f4589d9d18ec0fd289f2c98be57130415974
RuiHKUST <rzhouae@connect.ust.hk>
2019-07-02 20:52:56 +0800

    fix test example for stockDataDownload

--- tests/testthat/test-stockDataDownload.R
+++ tests/testthat/test-stockDataDownload.R
@@ -1,15 +1,10 @@
 context("Checking online data downloading")
 
-# stockDataDownload_check <- stockDataDownload(c("AAPL", "MSFT"), from = "2018-01-01", to = "2018-01-30")
-# stockDataDownload_check <- lapply(stockDataDownload_check, coredata)
-# save(stockDataDownload_check, file = "stockDataDownload_check.RData")
-
-load("stockDataDownload_check.RData")
-
 test_that("the embedded SP500 symbols are the same", {
   sink(file = tempfile())
-  stockData <- stockDataDownload(c("AAPL", "MSFT"), from = "2018-01-01", to = "2018-01-30")
+  stockData <- stockDataDownload(c("AAPL"), from = "2018-01-01", to = "2018-01-30")
   sink()
-  stockData <- lapply(stockData, coredata)
-  expect_equal(stockData, stockDataDownload_check)
+  stockData <- as.numeric(lapply(stockData, mean))
+  stockDataDownload_check <- quantmod::getSymbols("AAPL", from = "2018-01-01", to = "2018-01-30", auto.assign = FALSE)
+  expect_equal(as.numeric(lapply(stockData, mean)), as.numeric(colMeans(stockDataDownload_check)))
 })
