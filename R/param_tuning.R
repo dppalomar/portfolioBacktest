1e3dbea2eb4022a29d1cd41a0d9ca3fd60334200
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-08-19 08:59:38 +0800

    Remove use of viridis package and comment out old code.

--- R/param_tuning.R
+++ R/param_tuning.R
@@ -148,7 +148,7 @@ plotPerformanceVsParam <- function(bt_all_portfolios, params_subset = NULL, name
            p <- ggplot(portfolio_data, 
                        aes_string(x = names(params_grid[idx_numeric[1]]), y = names(params_grid[idx_numeric[2]]), fill = "score")) +
              geom_tile() +  # geom_raster()
-             viridis::scale_fill_viridis(name = name_performance, na.value = "transparent") +
+             scale_fill_viridis_c(name = name_performance, na.value = "transparent") +
              ggtitle(title_name)
            if (length(idx_factor) == 1)  # first factor to facets
              p <- p + facet_wrap(as.formula(paste("~", names(params_grid[idx_factor[1]]))), 
@@ -166,66 +166,66 @@ plotPerformanceVsParam <- function(bt_all_portfolios, params_subset = NULL, name
   
   
   
-plotPerformanceVsParam_old <- function(portfolio_funs, bt_summary, params_nominal = list(NULL), measure = "Sharpe ratio") {
-  score_all_funs <- summaryTable(bt_summary, measures = measure, type = "simple")
-  params_portfolio_funs <- lapply(portfolio_funs, attr, "params")
-  params_grid <- attr(portfolio_funs, "params_grid")
-  params_grid <- modifyList(params_grid, params_nominal)
-  N_grid <- sapply(params_grid, length)
-  switch(as.character(sum(N_grid > 1)),
-         "0" = stop("No parameter to be swept!"),
-         "1" = {
-           i_param1 <- which.max(N_grid)  # index of parameter to sweep
-           param1_sweep <- params_grid[[i_param1]]
-           param1_name <- names(params_grid)[i_param1]
-           score_sweep <- rep(NA, length(param1_sweep))
-           message("Sweeping over one single parameter: ", param1_name, ".")
-           for (i in 1:length(param1_sweep)) {
-             param_grid_point <- params_grid
-             param_grid_point[[i_param1]] <- param1_sweep[i]
-             i_fun <- which(sapply(params_portfolio_funs, identical, param_grid_point))
-             if (length(i_fun) > 1)
-               stop("More than one function in the list coincides with grid point ", param_grid_point)
-             if (length(i_fun) == 1)
-               score_sweep[i] <- score_all_funs[i_fun]
-           }
-           # plot
-           #plot(param1_sweep, score_sweep, type = "b", xlab = param1_name, ylab = "Sharpe ratio", 
-           #     main = "Performance vs parameter")
-           ggplot2::qplot(param1_sweep, score_sweep, geom = c("point", "line"), xlab = param1_name, ylab = "Sharpe ratio",
-                          main = "Performance vs parameter")
-         },
-         "2" = {
-           i_param1 <- which.max(N_grid)[1];            i_param2 <- order(N_grid, decreasing = TRUE)[2]
-           param1_sweep <- params_grid[[i_param1]];     param2_sweep <- params_grid[[i_param2]]
-           param1_name <- names(params_grid)[i_param1]; param2_name <- names(params_grid)[i_param2]
-           score_sweep <- matrix(NA, length(param1_sweep), length(param2_sweep))
-           rownames(score_sweep) <- param1_sweep
-           colnames(score_sweep) <- param2_sweep
-           message("Sweeping over two parameters: ", param1_name, " and ", param2_name, ".")
-           for (i in 1:length(param1_sweep))
-             for (j in 1:length(param2_sweep)) {
-               param_grid_point <- params_grid
-               param_grid_point[[i_param1]] <- param1_sweep[i]
-               param_grid_point[[i_param2]] <- param2_sweep[j]
-               i_fun <- which(sapply(params_portfolio_funs, identical, param_grid_point))
-               if (length(i_fun) > 1)
-                 stop("More than one function in the list coincides with grid point ", param_grid_point)
-               if (length(i_fun) == 1)
-                 score_sweep[i, j] <- score_all_funs[i_fun]
-             }
-           # plot
-           #heatmap(score_sweep, Rowv = NA, Colv = NA, col = heat.colors(256))
-           #heatmap(score_sweep, Rowv = NA, Colv = NA, col = viridisLite::inferno(256))
-           melted_score <- reshape2::melt(score_sweep)
-           ggplot2::ggplot(melted_score, ggplot2::aes(x = Var1, y = Var2, fill = value)) + 
-             ggplot2::geom_tile() + #ggplot2::geom_raster() +
-             ggplot2::labs(title = "Performance vs parameters", x = param1_name, y = param2_name) +
-             viridis::scale_fill_viridis(name = measure, na.value = "transparent")
-           #scale_fill_gradient(low = "white", high = "steelblue")
-         },
-         stop("Cannot sweep over more than 1 parameter."))
-}
+# plotPerformanceVsParam_old <- function(portfolio_funs, bt_summary, params_nominal = list(NULL), measure = "Sharpe ratio") {
+#   score_all_funs <- summaryTable(bt_summary, measures = measure, type = "simple")
+#   params_portfolio_funs <- lapply(portfolio_funs, attr, "params")
+#   params_grid <- attr(portfolio_funs, "params_grid")
+#   params_grid <- modifyList(params_grid, params_nominal)
+#   N_grid <- sapply(params_grid, length)
+#   switch(as.character(sum(N_grid > 1)),
+#          "0" = stop("No parameter to be swept!"),
+#          "1" = {
+#            i_param1 <- which.max(N_grid)  # index of parameter to sweep
+#            param1_sweep <- params_grid[[i_param1]]
+#            param1_name <- names(params_grid)[i_param1]
+#            score_sweep <- rep(NA, length(param1_sweep))
+#            message("Sweeping over one single parameter: ", param1_name, ".")
+#            for (i in 1:length(param1_sweep)) {
+#              param_grid_point <- params_grid
+#              param_grid_point[[i_param1]] <- param1_sweep[i]
+#              i_fun <- which(sapply(params_portfolio_funs, identical, param_grid_point))
+#              if (length(i_fun) > 1)
+#                stop("More than one function in the list coincides with grid point ", param_grid_point)
+#              if (length(i_fun) == 1)
+#                score_sweep[i] <- score_all_funs[i_fun]
+#            }
+#            # plot
+#            #plot(param1_sweep, score_sweep, type = "b", xlab = param1_name, ylab = "Sharpe ratio", 
+#            #     main = "Performance vs parameter")
+#            ggplot2::qplot(param1_sweep, score_sweep, geom = c("point", "line"), xlab = param1_name, ylab = "Sharpe ratio",
+#                           main = "Performance vs parameter")
+#          },
+#          "2" = {
+#            i_param1 <- which.max(N_grid)[1];            i_param2 <- order(N_grid, decreasing = TRUE)[2]
+#            param1_sweep <- params_grid[[i_param1]];     param2_sweep <- params_grid[[i_param2]]
+#            param1_name <- names(params_grid)[i_param1]; param2_name <- names(params_grid)[i_param2]
+#            score_sweep <- matrix(NA, length(param1_sweep), length(param2_sweep))
+#            rownames(score_sweep) <- param1_sweep
+#            colnames(score_sweep) <- param2_sweep
+#            message("Sweeping over two parameters: ", param1_name, " and ", param2_name, ".")
+#            for (i in 1:length(param1_sweep))
+#              for (j in 1:length(param2_sweep)) {
+#                param_grid_point <- params_grid
+#                param_grid_point[[i_param1]] <- param1_sweep[i]
+#                param_grid_point[[i_param2]] <- param2_sweep[j]
+#                i_fun <- which(sapply(params_portfolio_funs, identical, param_grid_point))
+#                if (length(i_fun) > 1)
+#                  stop("More than one function in the list coincides with grid point ", param_grid_point)
+#                if (length(i_fun) == 1)
+#                  score_sweep[i, j] <- score_all_funs[i_fun]
+#              }
+#            # plot
+#            #heatmap(score_sweep, Rowv = NA, Colv = NA, col = heat.colors(256))
+#            #heatmap(score_sweep, Rowv = NA, Colv = NA, col = viridisLite::inferno(256))
+#            melted_score <- reshape2::melt(score_sweep)
+#            ggplot2::ggplot(melted_score, ggplot2::aes(x = Var1, y = Var2, fill = value)) + 
+#              ggplot2::geom_tile() + #ggplot2::geom_raster() +
+#              ggplot2::labs(title = "Performance vs parameters", x = param1_name, y = param2_name) +
+#              viridis::scale_fill_viridis(name = measure, na.value = "transparent")
+#            #scale_fill_gradient(low = "white", high = "steelblue")
+#          },
+#          stop("Cannot sweep over more than 1 parameter."))
+# }
 
 
 
