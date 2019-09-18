20d44fccef5b76ef58c393c11353b6666d1070f6
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-09-18 18:52:34 +0800

    Minor changes.

--- R_buildignore/developer_commands.R
+++ R_buildignore/developer_commands.R
@@ -18,7 +18,7 @@ vignette(package = "portfolioBacktest")
 
 
 ##
-## Developer commands (http://r-pkgs.had.co.nz/)
+## Developer commands (https://r-pkgs.org/)
 ##
 devtools::load_all()  #or Ctrl-Shift-L
 devtools::install()
