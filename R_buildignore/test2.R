600798355b8dc8f32fb4d608d44016ae06d9257d
dppalomar <daniel.p.palomar@gmail.com>
2026-05-18 17:17:14 +0800

    Update version references and test script to 0.4.2

--- R_buildignore/test2.R
+++ R_buildignore/test2.R
@@ -2,10 +2,13 @@ library(portfolioBacktest)
 data("dataset10")  # load dataset
 
 # define your own portfolio function
-uniform_portfolio <- function(dataset) {
+uniform_portfolio <- function(dataset, ...) {
   N <- ncol(dataset$adjusted)
   return(rep(1/N, N))
 }
 
 # do backtest
 bt <- portfolioBacktest(list("Uniform" = uniform_portfolio), dataset10)
+
+
+backtestSummary(bt)$performance
